6898e56fc222457c5f996eb994b7ef9950f2b63a
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-23 00:56:00 -0400

    Add calculator button functionality

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 let num1 = '';
 let num2 = '';
 let opFlag = 0;
-let operator;
+let operator = '';
+
+//i will need to make the text add to a p tag inside the output so it can flex wrap
 
 function add(num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
     return num1 + num2;
 }
 
 function subtract(num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
     return num1 - num2;
 }
 
 function multiply(num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
     return num1 * num2;
 }
 
 function divide(num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
     return num1 / num2;
 }
 
@@ -35,8 +45,8 @@ function clear() {
 
 }
 
-const output = document.querySelector('.output');
-output.textContent = '0';
+const output = document.querySelector('.output p');
+output.textContent = '';
 
 const numButtons = document.querySelectorAll('.digits button');
 Array.from(numButtons).map((btn) => {
@@ -44,100 +54,135 @@ Array.from(numButtons).map((btn) => {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '1';
-                output.textContent = num1;
-                //will also need to set opflag to 1
             } else {
                 num2 += '1';
             }
+            output.textContent += '1';
         });
     } else if (btn.id === '2') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '2';
-                output.textContent = num1;
             } else {
                 num2 += '2';
             }
+            output.textContent += '2';
         });
     } else if (btn.id === '3') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '3';
-                output.textContent = num1;
-                //will have to update the text on screen too
+                
             } else {
                 num2 += '3';
             }
+            output.textContent += '3';
         });
     } else if (btn.id === '4') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '4';
-                output.textContent = num1;
-                //will have to update the text on screen too
             } else {
                 num2 += '4';
             }
+            output.textContent += '4';
         });
     } else if (btn.id === '5') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '5';
-                output.textContent = num1;
-                //will have to update the text on screen too
             } else {
                 num2 += '5';
             }
+            output.textContent += '5';
         });
     } else if (btn.id === '6') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '6';
-                output.textContent = num1;
-                //will have to update the text on screen too
             } else {
                 num2 += '6';
             }
+            output.textContent += '6';
         });
     } else if (btn.id === '7') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '7';
-                output.textContent = num1;
-                //will have to update the text on screen too
             } else {
                 num2 += '7';
             }
+            output.textContent += '7';
         });
     } else if (btn.id === '8') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '8';
-                output.textContent = num1;
-                //will have to update the text on screen too
             } else {
                 num2 += '8';
             }
+            output.textContent += '8';
         });
     } else if (btn.id === '9') {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '9';
-                output.textContent = num1;
-                //will have to update the text on screen too
             } else {
                 num2 += '9';
             }
+            output.textContent += '9';
         });
     } else {
         btn.addEventListener('click', () => {
             if (opFlag === 0) {
                 num1 += '0';
-                output.textContent = num1;
-                //will have to update the text on screen too
+                output.textContent += '0';
             } else {
                 num2 += '0';
             }
+        });
+    }
+});
+
+const operButtons = document.querySelectorAll('.operators button');
+Array.from(operButtons).map((btn) => {
+    if (btn.id === '/') {
+        btn.addEventListener('click', () => {
+            operator = '/';
+            output.textContent += ' / ';
+            opFlag = 1;
+        });
+    } else if (btn.id === '*') {
+        btn.addEventListener('click', () => {
+            operator = '*';
+            output.textContent += ' * ';
+            opFlag = 1;
+        });
+    } else if (btn.id === '-') {
+        btn.addEventListener('click', () => {
+            operator = '-';
+            output.textContent += ' - ';
+            opFlag = 1;
+        });
+    } else if (btn.id === '+') {
+        btn.addEventListener('click', () => {
+            operator = '+';
+            output.textContent += ' + ';
+            opFlag = 1;
+        });
+    } else if (btn.id === '=') {
+        btn.addEventListener('click', () => {
+            operator = '=';
+            operate(num1, num2, operator);
+            opFlag = 0;
+        });
+    } else {
+        btn.addEventListener('click', () => {
+            operator = '';
+            output.textContent = '';
+            opFlag = 0;
+            num1 = '';
+            num2 = '';
         });
     }
 });
